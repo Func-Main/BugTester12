42807fcc4e8cf1cc79e49d7bc3f77815b8169d30
Daniel Morgan <danmorgz@googlemail.com>
2021-10-05 11:34:56 +0100

    initial commit

---
+++ BugTester12/BugTester12App.swift
@@ -0,0 +1,17 @@
+//
+//  BugTester12App.swift
+//  BugTester12
+//
+//  Created by Daniel Morgan on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct BugTester12App: App {
+    var body: some Scene {
+        WindowGroup {
+            TestView()
+        }
+    }
+}
